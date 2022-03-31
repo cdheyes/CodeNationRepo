767610d2229fb6b4aac8113bf4d4400938a9f557
Chris Heyes <cdheyes@outlook.com>
2022-03-31 16:44:27 +0100

    v2 styled

--- diceGame/app.js
+++ diceGame/app.js
@@ -2,54 +2,64 @@
 const bust = 20;
 let total = 0;
 let dice = 0;
+let diceString = "";
 
 // initialise DOM links
 const numDisplay = document.getElementById('numDisplay');
 const totalDisplay = document.getElementById('totalDisplay');
 const rollClick = document.getElementById("rollClick");
+const diceImage = document.getElementById("diceImage");
 
 // button listener dice roller
 rollClick.addEventListener("click", (event) => {
-  dice = generateDiceNumber();
+  
+  // Dice roll and show
+  dice = Math.floor(Math.random() * 6) + 1;
   total += dice;
-  if (total <= bust) {
-    numDisplay.innerHTML = `Rolled a ${dice}.`;
-    totalDisplay.innerHTML = `Total is ${total}.`;
+  diceSwitchFace();
+  
+  // Game logic
+  if (dice == 1) {
+    numDisplay.innerHTML = `It's a 1!`;
+    totalDisplay.innerHTML = `YOU LOSE!`;
+    rollClick.innerHTML = "RESTART";
+    restartGame();
+  }
+  else if (total >= bust) {
+    numDisplay.innerHTML = `Rolled a ${dice}`;
+    totalDisplay.innerHTML = `Winner!`;
+    rollClick.innerHTML = "RESTART";
+    restartGame();
   }
   else {
-    numDisplay.innerHTML = `Rolled a ${dice}.`;
-    totalDisplay.innerHTML = `You have BUST!!!`;
-    total = 0;
-  };
+    numDisplay.innerHTML = `Rolled a ${dice}`;
+    totalDisplay.innerHTML = `${total}`;
+  }
 });
 
+// switch the dice image
+function diceSwitchFace() {
+    diceString = dice.toString();
+    switch (diceString) {
+    case "1": diceImage.src = `images/diceFace_1.png`;
+              break;
+    case "2": diceImage.src = `images/diceFace_2.png`;
+              break;
+    case "3": diceImage.src = `images/diceFace_3.png`;
+              break;
+    case "4": diceImage.src = `images/diceFace_4.png`;
+              break;
+    case "5": diceImage.src = `images/diceFace_5.png`;
+              break;
+    case "6": diceImage.src = `images/diceFace_6.png`;
+              break;
+    default: numDisplay.innerHTML = `this is not used`;
+  } 
+}
 
-function generateDiceNumber() {
-  return Math.floor(Math.random() * 6) + 1;
+function restartGame() {
+rollClick.addEventListener("click", (event) => {
+  location.reload();
+  });
 };
-
-
-// dice score function
-// function diceScore(dice) {
-//     switch (dice) {
-//     case "1": numDisplay.textContent = `Rolled a ${dice}. Total is ${total}`;
-//               // audio = new Audio('sounds/boom.wav');
-//               // audio.play(); 
-//               break;
-//     case "2": numDisplay.textContent = `2`;
-//               break;
-//     case "3": numDisplay.textContent = `3`;
-//               break;
-//     case "4": numDisplay.textContent = `4`;
-//               break;
-//     case "5": numDisplay.textContent = `5`;
-//               break;
-//     case "6": numDisplay.textContent = `6`;
-//               break;
-//     default: numDisplay.textContent = `${keyPressed} is not used`;
-//   } 
-// }
-
-
-
-
+// 
